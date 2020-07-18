4e86ab765e875a5ba59d4c8dbf55de945aea68b8
ArnaldoManjate12 <arnaldo@rapidtrade.biz>
2020-07-18 20:14:10 +0200

    got the mini app working

--- script.js
+++ script.js
@@ -1,85 +1,53 @@
+//  - your Error here was that you were trying to be too complicated 
+//  - it is good practice to write comments that explains you code for yourself and other developers
+//  - you also used very bad naming conventions ,your function names and variable names MUST be clear and concise
+//  - always plan yor work before writing a single line of code
 
-// ****Code wars Challenges****
+// Step 1 : creat a function that is only responsible for getting the user input 
+// Step 2 : create a function that is responsible for using that value returned from GetUserInput and changing the HTML
+// Step 3 : Listen for the Click Event
 
-/*
-code for calculating which quater of the year does each month fall
-under
- */
-
-// const quaterOF = (month) =>{
-//
-//   const formula = 1/3*month;
-//   const quater = Math.ceil(formula);
-//
-//   console.log(quater);
-// };
-//
-// quaterOF(12);
-
-/*
-Another code wars Challenges that totally works!!
-*/
+// Listen for the click event and call the GetUserInput function
 const submit = document.querySelector(".submit")
+submit.addEventListener('click',GetUserInput)
 
-submit.addEventListener('click', function(){
-  const getInputs = () =>{
-    const getOrder = document.querySelector(".input");
-    const order = getOrder.value;
-    const usersChoice = document.querySelector("#usersChoice");
-    const choice = usersChoice.value;
-    alert(order);
-    return order;
-    return choice;
-  };
-
-  const readyOrder = () => {
-    getInputs()
-
-    let drink;
-   switch (drink) {
-     case order === "JABRONI":
-       drink = "Patron Tequila";
-       break;
-     case order === "SCHOOL COUNSELOR":
-       drink = "Anything with Alcohol";
-       break;
-     case order === "PROGRAMMER":
-       drink = "Hipster craft beer";
-       break;
-     case order === "BIKE GANG MEMEBER":
-       drink = "Moonshine";
-       break;
-     case order === "POLITICIAN":
-       drink = "Your tax dollars";
-       break;
-     case order === "RAPPER":
-       drink = "Cristal";
-       break;
-     default:
-       drink = "Beer";
-   }
-
-   return drink;
-  };
-
- const returnOutput = () =>{
-  const output = usersChoice === drink;
-  return output;
-
- };
-
-  returnOutput(readyOrder());
-});
-
-
-  // learn how to here : https://www.w3schools.com/jsref/met_document_queryselector.asp
-  // get the element where the user's choice is shown ie:  document.querySelactor(#usersChoice)
-  // declare an empty variable ie : let newCoice = null;
-
- // use switch case statement instaed of if else  : ie:
-
-    // learn more about switch statement here : https://www.w3schools.com/js/js_switch.asp
-
-
-  // change the value of the usersChoice html to the value of newChoice so the html changes when the user presses the button
-  // learn how to here : https://www.w3schools.com/jsref/prop_html_innerhtml.asp
+// GetUserInput is responsible for retrieving the value entered by the user and passes 
+// passes that value in as a argument of ReturnUsersDrink
+function GetUserInput(){
+  // get the vale the user inputed and convert it to uppercase
+  const valueFromUser = document.querySelector(".input").value;
+  valueFromUser.toUpperCase()
+  // return the drink
+  ReturnUsersDrink(valueFromUser);
+}
+// ReturnUsersDrink is responsible for recieving the value the user typed in as an Argument and returning the relevalt drink
+// if a Users choice is invalid "Beer" will be returned
+function ReturnUsersDrink(valueFromUser) {
+  let drink;
+  switch (valueFromUser) {
+    case "JABRONI": 
+      drink = "Patron Tequila";
+      break;
+    case "SCHOOL COUNSELOR":
+      drink = "Anything with Alcohol";
+      break;
+    case "PROGRAMMER":
+      drink = "Hipster craft beer";
+      break;
+    case "BIKE GANG MEMEBER":
+      drink = "Moonshine";
+      break;
+    case "POLITICIAN":
+      drink = "Your tax dollars";
+      break;
+    case "RAPPER":
+      drink = "Cristal";
+      break;
+    default:
+      drink = "Beer";
+  }
+  // select the User's Choice Div
+  const outptDiv = document.querySelector("#users-choice");
+  // change the html
+  outptDiv.innerHTML = "Enjoy Your " + drink
+}
